b5afc9042521c7273849b058c276e50bc70c4484
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2023-07-13 19:50:55 -0400

    Add simple HTTP example

---
+++ examples/http/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/picatz/jose/pkg/header"
+	"github.com/picatz/jose/pkg/jwa"
+	"github.com/picatz/jose/pkg/jwt"
+)
+
+func main() {
+	token, err := jwt.New(
+		header.Parameters{
+			header.Type:      header.TypeJWT,
+			header.Algorithm: jwa.HS256,
+		}, jwt.ClaimsSet{
+			"sub":  "1234567890",
+			"name": "John Doe",
+		},
+		"supersecret",
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(token)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		bearerToken := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+
+		token, err := jwt.ParseString(bearerToken)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = token.Verify(jwt.SecretKey("supersecret"))
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	fmt.Println("Listening on http://0.0.0.0:8080")
+	panic(http.ListenAndServe(":8080", mux))
+}
