f405a1b7740d2f0916f2c32eb905818bd1596fdc
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2024-08-24 10:45:29 -0400

    Iterate on the `header` package

--- pkg/header/header_test.go
+++ pkg/header/header_test.go
@@ -9,17 +9,111 @@ import (
 )
 
 func TestJSONDecode(t *testing.T) {
-	input := `{"typ":"JWT","alg":"HS256"}`
+	tests := []struct {
+		name  string
+		input string
+		check func(t *testing.T, params Parameters)
+	}{
+		{
+			name:  "typ and alg",
+			input: `{"typ":"JWT","alg":"HS256"}`,
+			check: func(t *testing.T, params Parameters) {
+				typ, err := params.Type()
+				require.NoError(t, err)
+				require.Equal(t, "JWT", typ)
 
-	params := Parameters{}
-	err := json.NewDecoder(strings.NewReader(input)).Decode(&params)
-	require.NoError(t, err)
+				alg, err := params.Algorithm()
+				require.NoError(t, err)
+				require.Equal(t, "HS256", alg)
+			},
+		},
+		{
+			name:  "typ and alg and kid",
+			input: `{"typ":"JWT","alg":"HS256","kid":"key-id"}`,
+			check: func(t *testing.T, params Parameters) {
+				typ, err := params.Type()
+				require.NoError(t, err)
+				require.Equal(t, "JWT", typ)
 
-	typ, err := params.Type()
-	require.NoError(t, err)
-	require.Equal(t, "JWT", typ)
+				alg, err := params.Algorithm()
+				require.NoError(t, err)
+				require.Equal(t, "HS256", alg)
 
-	b64urlStr, err := params.Base64URLString()
-	require.NoError(t, err)
-	require.NotEmpty(t, b64urlStr)
+				kid, err := params.Get(KeyID)
+				require.NoError(t, err)
+				require.Equal(t, "key-id", kid)
+			},
+		},
+		{
+			name:  "typ and alg and kid and crit",
+			input: `{"typ":"JWT","alg":"HS256","kid":"key-id","crit":["exp","nbf"]}`,
+			check: func(t *testing.T, params Parameters) {
+				typ, err := params.Type()
+				require.NoError(t, err)
+				require.Equal(t, "JWT", typ)
+
+				alg, err := params.Algorithm()
+				require.NoError(t, err)
+				require.Equal(t, "HS256", alg)
+
+				kid, err := params.Get(KeyID)
+				require.NoError(t, err)
+				require.Equal(t, "key-id", kid)
+
+				crit, err := params.Get(Critical)
+				require.NoError(t, err)
+				require.Equal(t, []any{"exp", "nbf"}, crit)
+			},
+		},
+		{
+			name:  "missing typ",
+			input: `{"alg":"HS256"}`,
+			check: func(t *testing.T, params Parameters) {
+				typ, err := params.Type()
+				require.Error(t, err)
+				require.ErrorIs(t, err, ErrParamaterNotFound)
+				require.Equal(t, "", typ)
+			},
+		},
+		{
+			name:  "missing alg",
+			input: `{"typ":"JWT"}`,
+			check: func(t *testing.T, params Parameters) {
+				alg, err := params.Algorithm()
+				require.Error(t, err)
+				require.ErrorIs(t, err, ErrParamaterNotFound)
+				require.Equal(t, "", alg)
+			},
+		},
+		{
+			name:  "invalid typ",
+			input: `{"typ":123,"alg":"HS256"}`,
+			check: func(t *testing.T, params Parameters) {
+				typ, err := params.Type()
+				require.Error(t, err)
+				require.ErrorIs(t, err, ErrInvalidParamaterType)
+				require.Equal(t, "", typ)
+			},
+		},
+		{
+			name:  "invalid alg",
+			input: `{"typ":"JWT","alg":123}`,
+			check: func(t *testing.T, params Parameters) {
+				alg, err := params.Algorithm()
+				require.Error(t, err)
+				require.ErrorIs(t, err, ErrInvalidParamaterType)
+				require.Equal(t, "", alg)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var params Parameters
+			err := json.NewDecoder(strings.NewReader(test.input)).Decode(&params)
+			require.NoError(t, err)
+
+			test.check(t, params)
+		})
+	}
 }
